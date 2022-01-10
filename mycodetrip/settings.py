b245d58cbed2aebeb2a0093792f6eefcdd730751
이민석 <minxi@iminseogs-MacBook-Air.local>
2022-01-10 18:48:20 +0900

    프로젝트 초기 세팅 수정

--- mycodetrip/settings.py
+++ mycodetrip/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from my_settings import SECRET_KEY, DATABASES
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'co-kr'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
