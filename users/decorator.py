7f84c2baf7ddb943cfc4e8532334971a8a10585b
Chanjoo lee <dlckswn14@gmail.com>
2022-01-17 00:46:13 +0900

    로그인 데코레이터 구현

---
+++ users/decorator.py
@@ -0,0 +1,30 @@
+import jwt
+from jwt.exceptions import DecodeError, ExpiredSignatureError
+from django.http    import JsonResponse
+
+from mycodetrip.config  import SECRET_KEY
+from users.models       import User
+
+def login_required(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers["Authorization"]
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithms="HS256")
+            user         = User.objects.get(id=payload.get("id", 0))
+            request.user = user
+
+        except KeyError:
+            return JsonResponse({"message":"NO_TOKEN"}, status=400)
+
+        except DecodeError:
+            return JsonResponse({"message": "INVALID_TOKEN"}, status=400)
+
+        except ExpiredSignatureError:
+            return JsonResponse({"message": "EXPIRED_TOKEN"}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({"MESSAGE": "INVALID_USER"}, status=400)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
