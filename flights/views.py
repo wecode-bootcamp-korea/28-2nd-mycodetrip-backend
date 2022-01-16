3008decb8dded2a4a10c1b64646a3008f7a4467f
Chanjoo lee <dlckswn14@gmail.com>
2022-01-16 19:26:20 +0900

    필터링 기능 추가 및 예외처리

--- flights/views.py
+++ flights/views.py
@@ -1,86 +1,145 @@
+import pytz
 from datetime import datetime, timedelta
-from django.http import JsonResponse
 
+from django.http import JsonResponse
 from django.views import View
 from django.db.models import Q
 
-from flights.models import *
+from flights.models import Flight, FlightSeat, Airline, City, Category, Seat
 
 
 class FlightListView(View):
     def get(self, request):
         # 필수 쿼리파라미터
-        departure_date   = request.GET.get("departure_date") # 여행 시작
-        arrival_date     = request.GET.get("arrival_date") # 여행 끝
-        departure_city   = request.GET.get("departure_city") # "김포"
-        arrival_city     = request.GET.get("arrival_city") # "제주"
-        is_round_trip    = request.GET.get("is_round_trip") # 왕복?
-        number_of_person = request.GET.get("number_of_person") # 성인 1 명
+        try:
+            departure_date = request.GET["departure_date"] # "2022-02-27"
+            arrival_date   = request.GET["arrival_date"]
+            departure_city = request.GET["departure_city"]
+            arrival_city   = request.GET["arrival_city"]
+        except KeyError:
+            return JsonResponse({"message":"REQUIRED_PARAMETER_MISSING"})
+
         # 선택 쿼리파라미터
-        airline_id_list  = request.GET.getlist("airline", "all") # 항공사 [1,3,4]
-        at_time          = request.GET.getlist("during_time", "all") # 00:00~06:00 => [0,6,12,18]
-        seat_type        = request.GET.getlist("seat_type", "all") # 좌석 등급
-        sorting          = request.GET.get("sort", "낮은가격순")
-        print(departure_city)
-        print(arrival_city)
-        sort = {
-            "낮은가격순":"price",
-            # "출발시간빠른순":"departure_time",
-            # "출발시간느린순":"-departure_time",
+        airline_list = request.GET.getlist("airline_list")
+        at_time      = request.GET.getlist("at_time") # ['0','6','12','18'], 없을땐 [] 빈리스트 반환
+        seat_type    = request.GET.getlist("seat_type")
+        sorting      = request.GET.get("sorting", "lower_price")
+        max_price    = request.GET.get("max_price", None)
+        offset       = int(request.GET.get("offset", 0))
+        limit        = int(request.GET.get("limit", 20))
+
+        # timezone 정보 포함한 aware datetime 객체
+        KST = pytz.timezone('Asia/Seoul')
+        year, month, day = map(int,departure_date.split("-"))
+        aware_datetime = datetime(year,month,day, tzinfo=KST)
+
+        TIME = {
+            0: aware_datetime,
+            6: aware_datetime+timedelta(hours=6),
+            12: aware_datetime+timedelta(hours=12),
+            18: aware_datetime+timedelta(hours=18),
+            24: aware_datetime+timedelta(hours=24)
         }
 
-        # departure_date = str(datetime.strptime(departure_date, "%Y-%m-%d")) # 2022-02-14
-        # arrival_date   = str(datetime.strptime(arrival_date, "%Y-%m-%d"))
+        SORT = {
+            "lower_price"      : "flightseat__price", # 테이블명 소문자
+            "earlier_departure": "departure_time",
+            "late_departure"   : "-departure_time",
+        }
 
-        from_city = City.objects.filter(name=departure_city).first()
-        to_city   = City.objects.filter(name=arrival_city).first()
+        try:
+            from_city = City.objects.get(name=departure_city)
+            to_city   = City.objects.get(name=arrival_city)
+        except City.DoesNotExist:
+            return JsonResponse({"message":"INVALID_CITY"}, status=400)
+
+        '''
+        <SQL>
+        SELECT * FROM flights
+        WHERE
+            departure_city_id = 2 AND
+            arrival_city=3 AND
+            (
+                departure_time between time[0] AND time[6] OR
+                departure_time between time[12] AND time[18]
+            );
+        '''
 
         q = Q()
-        q &= Q(departure_time__gte=departure_date, departure_city=from_city.id, arrival_city=to_city.id)
+        q &= Q(departure_city=from_city.id, arrival_city=to_city.id)
+        q &= Q(departure_time__range=(TIME[0], TIME[24]))
 
-        if airline_id_list:
-            pass
         if at_time:
-            pass
+            time_q = Q()
+
+            if '0' in at_time:
+                time_q |= Q(departure_time__range=(TIME[0], TIME[6]))
+            if '6' in at_time:
+                time_q |= Q(departure_time__range=(TIME[6], TIME[12]))
+            if '12' in at_time:
+                time_q |= Q(departure_time__range=(TIME[12], TIME[18]))
+            if '18' in at_time:
+                time_q |= Q(departure_time__range=(TIME[18], TIME[24]))
+
+            q &= time_q
+
+        try:
+            airline_list = [int(airline) for airline in airline_list]
+            seat_type = [int(type) for type in seat_type]
+            if max_price:
+                max_price = int(max_price)
+        except ValueError:
+            return JsonResponse({"message":"INVALID_INPUT"}, status=400)
+
+
+        if airline_list:
+            q &= Q(aircraft__airline__id__in=airline_list)
         if seat_type:
-            pass
+            q &= Q(flightseat__seat_id__in=seat_type)
+        if max_price:
+            q &= Q(flightseat__price__lte=max_price)
 
-        flights = Flight.objects.filter(q).select_related("departure_city","arrival_city","aircraft")
+        flights_query = Flight.objects.filter(q).order_by(SORT[sorting])
+        flights = flights_query.select_related("departure_city","arrival_city","aircraft")[offset:offset+limit]
 
-        result = {
-            "total": flights.count(),
-            "departure_date": departure_date,
-            "arrival_date": arrival_date,
-            "flight_list": [{
-                "id": flight.id, # 항공편 pk
-                "flight_time": flight.flight_time, # 비행 시간
-                "airline"    : flight.aircraft.airline.name, # 항공사
-                "aircraft"   : flight.aircraft.code, # 항공기
-                "logo"       : flight.aircraft.airline.logo,
-                "price"      : int(FlightSeat.objects.filter(flight=flight).first().price),
-                "tickets"    : FlightSeat.objects.filter(flight=flight).first().stock,
-                "seat_type"  : FlightSeat.objects.filter(flight=flight).first().seat_id, # 좌석 등급
-                "departure": {
-                    "time": flight.departure_time, # 출발 시간
-                    "city": flight.departure_city.name, # 출발 도시명
-                    "code": flight.departure_city.code, # 출발 도시 코드
-                },
-                "arrival": {
-                    "time": flight.arrival_time, # 도착 시간
-                    "city": flight.arrival_city.name, # 도착 도시명
-                    "code": flight.arrival_city.code, # 도착 도시 코드
-                },
-            } for flight in flights],
-        }
-        return JsonResponse({"result":result}, status=200)
+        try:
+            result = {
+                "total": flights_query.count(), # 총 항공편 개수
+                "departure_date": departure_date,
+                "arrival_date": arrival_date,
+                "type": "flight_list",
+                "data": [{
+                    "id": flight.id, # 항공편 pk
+                    "flight_time": flight.flight_time, # 비행 시간
+                    "airline"    : flight.aircraft.airline.name, # 항공사
+                    "aircraft"   : flight.aircraft.code, # 항공기
+                    "logo"       : flight.aircraft.airline.logo,
+                    "price"      : int(FlightSeat.objects.get(flight=flight).price),
+                    "tickets"    : FlightSeat.objects.get(flight=flight).stock, # 구매 가능한 티켓 수
+                    "seat_type"  : FlightSeat.objects.get(flight=flight).seat_id, # 좌석 등급
+                    "departure": {
+                        "time": flight.departure_time, # 출발 시간
+                        "city": flight.departure_city.name, # 출발 도시명
+                        "code": flight.departure_city.code, # 출발 도시 코드
+                    },
+                    "arrival": {
+                        "time": flight.arrival_time, # 도착 시간
+                        "city": flight.arrival_city.name, # 도착 도시명
+                        "code": flight.arrival_city.code, # 도착 도시 코드
+                    },
+                } for flight in flights],
+            }
+            return JsonResponse({"result":result}, status=200)
+
+        except FlightSeat.DoesNotExist:
+            return JsonResponse({"message":"FLIGHTSEAT_DOES_NOT_EXIST"}, status=400)
 
 class CityListView(View):
     def get(self, request):
-
-        categories = Category.objects.select_related("city")
-
+        categories = Category.objects.all()
         result = {
-            "category":[{
+            "type": "city_list",
+            "data":[{
                 "id": category.id,
                 "category": category.name,
                 "city": [city for city in category.city_set.values()]
@@ -91,9 +150,9 @@ class CityListView(View):
 class AirlineListView(View):
     def get(self, request):
         airlines = Airline.objects.all()
-
         result = {
-            "airline_list": [{ # 항공사 리스트
+            "type": "airline_list",
+            "data": [{
                 "id": airline.id,
                 "name": airline.name,
                 "logo": airline.logo,
@@ -104,11 +163,11 @@ class AirlineListView(View):
 class SeatTypeView(View):
     def get(self, request):
         seats = Seat.objects.all()
-
         result = {
-            "seat_list": [{ # 좌석 등급 리스트
+            "type": "seat_type",
+            "data": [{
                 "id"  : seat.id,
-                "type": seat.type,
+                "name": seat.type,
             } for seat in seats],
         }
         return JsonResponse({"result":result}, status=200)
