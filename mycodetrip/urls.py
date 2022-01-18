bf9ef03a9073f4375d623918ede6dfb9ea514959
이민석 <minxi@iminseogs-MacBook-Air.local>
2022-01-18 14:35:57 +0900

    메인페이지 수정

--- mycodetrip/urls.py
+++ mycodetrip/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("flights", include("flights.urls")),
+    path('flights', include('flights.urls')),
+    path('orders', include('orders.urls'))
 ]
